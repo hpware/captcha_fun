39aafe3c9c450138975c6ad6244f2f76dbf1c9f0
Howard <hw@hwtw.cc>
2024-10-02 01:58:40 +0800

    captcha_fun

--- src/Pages/HCaptcha.vue
+++ src/Pages/HCaptcha.vue
@@ -1,10 +1,49 @@
 <script setup>
+// Import
+import VueHcaptcha from '@hcaptcha/vue3-hcaptcha';
+import { ref } from 'vue';
+const hardnesskey = ref('');
+const score = ref(0);
+const time = ref(0);
+// Import Keys
+const hardkey = import.meta.env.VITE_hCaptcha_Site_Key_Hard;
+const modkey = import.meta.env.VITE_hCaptcha_Site_Key_Moderate;
+const easykey = import.meta.env.VITE_hCaptcha_Site_Key_Easy;
+const hardness = new URLSearchParams(window.location.search).get("hns");
+const selected = ref(hardness);
+if (!hardness) {
+  selected.value = 'no';
+}
+if (hardness === 'easy' | hardness === 'mod' | hardness === 'hard') {
+  selected.value = 'yes';
+  if (hardness === 'easy') {
+    hardnesskey.value = easykey;
+  } else if (hardness === 'mod') {
+    hardnesskey.value = modkey;
+  } else if (hardness === 'hard') {
+    hardnesskey.value = hardkey;
+  }
+}
+// Captcha
+const onCaptchaVerified = (token) => {
+  score.value = score.value + 1;
+};
 
 </script>
 
 <template>
   <h2>HCaptcha</h2>
-  <h3>Timer: <span id="timer">null</span><br>Score: <span id="score">null</span></h3>
+  <div v-if="selected === 'no'">
+    <h3>Choose a difficulty</h3>
+    <a href="/hcaptcha?hns=easy">Easy</a>&nbsp;
+    <a href="/hcaptcha?hns=mod">Moderate</a>&nbsp;
+    <a href="/hcaptcha?hns=hard">Hard</a>
+  </div>
+  <div v-if="selected === 'yes'">
+    <h3>Timer: <span id="timer">{{ time }}</span><br>Score: <span id="score">{{ score }}</span></h3>
+  <vue-hcaptcha sitekey="${{hardnesskey}}" @verify="onCaptchaVerified"></vue-hcaptcha>
+
+  </div>
 </template>
 
 <style scoped>
--- src/Pages/Index.vue
+++ src/Pages/Index.vue
@@ -1,9 +1,9 @@
 <script setup>
 import  { ref } from 'vue';
-
 </script>
 
 <template>
+  <title>Home | Captcha Fun!</title>
   <h2>Home</h2>
   <p>Welcome to the home page of captcha_fun</p>
   <a href="/gcaptcha">Google Captcha</a>&nbsp;
