ee7764bad01a0bcd81d795484977ae94c39927f9
Howard <hw@hwtw.cc>
2024-10-02 10:01:21 +0800

    Haled completetion i guess

--- src/Pages/HCaptcha.vue
+++ src/Pages/HCaptcha.vue
@@ -1,10 +1,18 @@
 <script setup>
 // Import
 import VueHcaptcha from '@hcaptcha/vue3-hcaptcha';
-import { ref } from 'vue';
+import cookies from 'vue-cookies';
+import { ref, onUnmounted } from 'vue';
 const hardnesskey = ref('');
 const score = ref(0);
+if (cookies.get('score')) {
+  score.value = Math.round(cookies.get('score'));
+}
 const time = ref(0);
+if (cookies.get('time')) {
+  time.value = Math.round(cookies.get('time'));
+}
+const captchaRef = ref(null);
 // Import Keys
 const hardkey = import.meta.env.VITE_hCaptcha_Site_Key_Hard;
 const modkey = import.meta.env.VITE_hCaptcha_Site_Key_Moderate;
@@ -14,7 +22,7 @@ const selected = ref(hardness);
 if (!hardness) {
   selected.value = 'no';
 }
-if (hardness === 'easy' | hardness === 'mod' | hardness === 'hard') {
+if (['easy', 'mod', 'hard'].includes(hardness)) {
   selected.value = 'yes';
   if (hardness === 'easy') {
     hardnesskey.value = easykey;
@@ -24,11 +32,31 @@ if (hardness === 'easy' | hardness === 'mod' | hardness === 'hard') {
     hardnesskey.value = hardkey;
   }
 }
-// Captcha
-const onCaptchaVerified = (token) => {
-  score.value = score.value + 1;
-};
+if (selected.value === 'no') {
+  cookies.remove('score');
+  cookies.remove('time');
+} else if (selected.value === 'yes') {
+  const interval = setInterval(() => {
+    time.value = Math.floor(time.value + 1);
+    cookies.set('time', time.value);
+    console.log(time.value);
+  }, 1000);
+  onUnmounted(() => {
+      clearInterval(interval);
+  });
+}
 
+// Captcha
+const onCaptchaVerified = () => {
+  score.value = Math.floor(score.value + 1);
+  cookies.set('score', score.value);
+  captchaRef.value.resetCaptcha(true);
+
+};
+const onCaptchaReset = () => {
+  score.value = Math.floor(score.value - 10);
+  cookies.set('score', score.value);
+};
 </script>
 
 <template>
@@ -40,8 +68,9 @@ const onCaptchaVerified = (token) => {
     <a href="/hcaptcha?hns=hard">Hard</a>
   </div>
   <div v-if="selected === 'yes'">
-    <h3>Timer: <span id="timer">{{ time }}</span><br>Score: <span id="score">{{ score }}</span></h3>
-  <vue-hcaptcha sitekey="${{hardnesskey}}" @verify="onCaptchaVerified"></vue-hcaptcha>
+    <h3>Timer: <span id="timer">{{ time }}s</span><br>Score: <span id="score">{{ score }}</span></h3>
+    <p style="color: red; font-size: 0.9em">Resets wiil -10 points</p>
+  <vue-hcaptcha ref="captchaRef" :sitekey="hardnesskey" @verify="onCaptchaVerified" @reset="onCaptchaReset"></vue-hcaptcha>
 
   </div>
 </template>
